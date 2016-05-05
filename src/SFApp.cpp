2e0503948da2abc5eec028873788112e31eb3da3
Nathan Hart (student) <nch15@moco207L-a4.university.brighton.ac.uk>
2016-05-05 16:01:42 +0100

    Added walls on the y axis, shifted position of enemies

--- src/SFApp.cpp
+++ src/SFApp.cpp
@@ -13,7 +13,7 @@ SFApp::SFApp(std::shared_ptr<SFWindow> window) : fire(0), is_running(true), sf_w
   for(int i=0; i<number_of_aliens; i++) {
     // place an alien at width/number_of_aliens * i
     auto alien = make_shared<SFAsset>(SFASSET_ALIEN, sf_window);
-    auto pos   = Point2((canvas_w/number_of_aliens) * i, 200.0f);
+    auto pos   = Point2((canvas_w/number_of_aliens) * i+30, 200.0f);
     alien->SetPosition(pos);
     aliens.push_back(alien);
   }
@@ -25,6 +25,24 @@ const int number_of_wallsx = 20;
     auto pos   = Point2((canvas_w/number_of_wallsx) * i, 5.0f);
     wallsx->SetPosition(pos);
     wallx.push_back(wallsx);
+  }
+
+const int number_of_wallsy = 20;
+  for(int i=0; i<number_of_wallsy; i++) {
+    // place an alien at width/number_of_aliens * i
+    auto wallsy = make_shared<SFAsset>(SFASSET_WALLY, sf_window);
+    auto pos   = Point2(5.0f, (canvas_w/number_of_wallsy) * i);
+    wallsy->SetPosition(pos);
+    wally.push_back(wallsy);
+  }
+
+const int number_of_wallsyright = 20;
+  for(int i=0; i<number_of_wallsyright; i++) {
+    // place an alien at width/number_of_aliens * i
+    auto wallsy = make_shared<SFAsset>(SFASSET_WALLY, sf_window);
+    auto pos   = Point2(635.0f, (canvas_w/number_of_wallsyright) * i);
+    wallsy->SetPosition(pos);
+    wally.push_back(wallsy);
   }
 
 const int number_of_wallsxtop = 20;
@@ -118,6 +136,7 @@ void SFApp::OnUpdateWorld() {
       if(player->CollidesWith(wx)) {
         player->HandleCollision();
         wx->HandleCollision();	
+	player->Stop();
 	std::cout<<"Player Collides with wall"<<std::endl;
       }
     
--- src/SFAsset.h
+++ src/SFAsset.h
@@ -37,6 +37,7 @@ public:
   virtual void      GoWest();
   virtual void      GoNorth();
   virtual void      GoSouth();
+  virtual void	    Stop();
   virtual void      SetNotAlive();
   virtual bool      IsAlive();
   virtual void      HandleCollision();
